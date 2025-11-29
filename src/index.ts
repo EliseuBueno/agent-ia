e0a7c901789a8c6ba6819a498badc247547f5024
eliabsbueno <eliabsbueno@gmail.com>
2025-11-29 14:00:43 -0300

    ajuste para looger

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import { BaseAgent } from "./agents/BaseAgent";
 import dotenv from "dotenv";
+import { sendLog } from "./uteis/logger";
 dotenv.config();
 
 async function run() {
   const agent = new BaseAgent();
   const reply = await agent.ask("Agora diga 'estou pronto para evoluir'");
-  console.log(reply);
+  sendLog.info(`${reply}`);
 }
 
 run();
